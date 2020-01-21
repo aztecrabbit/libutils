632b24e7256911820172d2d61093f8babc1c88a5
Aztec Rabbit <ars.xda@gmail.com>
2020-01-21 19:27:18 +0700

    ...

--- libutils.go
+++ libutils.go
@@ -3,8 +3,39 @@ package libutils
 import (
 	"os"
 	"os/signal"
+	"io/ioutil"
+	"path/filepath"
 	"syscall"
+	"encoding/json"
 )
+
+var (
+	PathFile string
+)
+
+func RealPath(name string) string {
+	return filepath.Dir(PathFile) + "/" + name
+}
+
+func JsonReadWrite(filename string, v interface{}, vd interface{}) {
+	r, err := os.Open(filename)
+	if err != nil {
+		JsonWrite(vd, filename)
+		r, _ = os.Open(filename)
+	}
+
+	bytedata, _ := ioutil.ReadAll(r)
+
+	json.Unmarshal(bytedata, v)
+}
+
+func JsonWrite(v interface{}, filename string) {
+	bytedata, _ := json.MarshalIndent(v, "", "    ")
+
+	ioutil.WriteFile(filename, bytedata, 0644)
+}
+
+//
 
 type InterruptHandler struct {
 	Handle func()
@@ -19,6 +50,6 @@ func (i *InterruptHandler) Start() {
     	if i.Handle != nil {
     		i.Handle()
 	    }
-    	os.Exit(0)
+    	os.Exit(1)
     }()
 }
